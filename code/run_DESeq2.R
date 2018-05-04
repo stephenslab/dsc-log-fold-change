101354bcbccc48e62fbfa75f9a262417fa80ac08
jhsiao999 <joyce.hsiao1@gmail.com>
2018-05-04 09:50:04 -0500

    enable parallel computing

--- code/run_DESeq2.R
+++ code/run_DESeq2.R
@@ -12,7 +12,14 @@ run_DESeq2 <- function(Y1, Y2) {
   dds <- DESeqDataSetFromMatrix(countData = round(Y),
                                 colData = data.frame(condition = x),
                                 design = ~condition)
-  dds <- DESeq(dds)
+
+  ncores_default <- detectCores()
+  if (ncores_default == 8) {
+    register(MulticoreParam(4))
+    } else {
+    register(MulticoreParam(ncores_default))
+    }
+  dds <- DESeq(dds, parallel = TRUE)
   res <- results(dds, contrast = c("condition", levels(factor(x))[1],
                                    levels(factor(x))[2]), alpha = 0.05);
 
