16474bf46b646f7e533a369daf82cab459ce3dab
Joyce Hsiao <joyce.hsiao1@gmail.com>
2019-04-22 23:27:15 -0500

    error handling: output NA values for each gene

--- dsc/modules/sva_voom.R
+++ dsc/modules/sva_voom.R
@@ -5,15 +5,23 @@ sva_voom <- function(Y, X, num_sv=NULL) {
     num_sv <- sva::num.sv(dat = Y, mod = X)
     cat("num_sv = ", num_sv, "\n")
   }
-  trash      <- capture.output(sva_out <- sva::sva(dat = Y, mod = X, n.sv = num_sv))
-  X.sv       <- cbind(X, sva_out$sv)
-  voom_out   <- limma::voom(counts = Y, design = X.sv)
-  limma_out  <- limma::lmFit(object = voom_out)
-  ebayes_out <- limma::ebayes(fit = limma_out)
-  betahat    <- limma_out$coefficients[, 2]
-  sebetahat  <- sqrt(ebayes_out$s2.post) * limma_out$stdev.unscaled[, 2]
-  df         <- ebayes_out$df.total[1]
-  pvalues    <- ebayes_out$p.value[, 2]
+#  trash      <- capture.output(sva_out <- sva::sva(dat = Y, mod = X, n.sv = num_sv))
+  try_sva      <- tryCatch(sva_out <- sva::sva(dat = Y, mod = X, n.sv = num_sv), error = function(e) "error")
+  if (try_sva=="error")  {
+    pvalues <- rep(NA, nrow(Y))
+    betahat <- rep(NA, nrow(Y))
+    sebetahat <- rep(NA, nrow(Y))
+    df <- rep(NA, nrow(Y))
+  } else {
+    X.sv       <- cbind(X, sva_out$sv)
+    voom_out   <- limma::voom(counts = Y, design = X.sv)
+    limma_out  <- limma::lmFit(object = voom_out)
+    ebayes_out <- limma::ebayes(fit = limma_out)
+    betahat    <- limma_out$coefficients[, 2]
+    sebetahat  <- sqrt(ebayes_out$s2.post) * limma_out$stdev.unscaled[, 2]
+    df         <- ebayes_out$df.total[1]
+    pvalues    <- ebayes_out$p.value[, 2]
+  }
   return(list(betahat = betahat, sebetahat = sebetahat, df = df,
               pvalues = pvalues))
 }
