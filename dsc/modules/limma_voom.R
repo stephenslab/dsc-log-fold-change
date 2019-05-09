4fb8349894f9c146849ad123f951f6611f59d7a5
Joyce Hsiao <joyce.hsiao1@gmail.com>
2019-05-09 17:09:40 -0500

    minor edits

--- dsc/modules/limma_voom.R
+++ dsc/modules/limma_voom.R
@@ -1,28 +1,13 @@
-limma_voom <- function(Y, X, normalize_counts=F){
+limma_voom <- function(Y, X){
 
   library(limma)
   library(edgeR)
-  #  Y <- as.matrix(cbind(Y1, Y2))
-  #  condition <- c(rep(1, ncol(Y1)), rep(2, ncol(Y2)))
 
-  if (normalize_counts) {
-    design <- X
-    Y <- DGEList(Y)
-    dge <- edgeR::calcNormFactors(Y)
-    v <- voom(dge,design,plot=FALSE)
-    fit <- lmFit(v,design)
-    fit.ebayes <- eBayes(fit)
-  }
-
-  # input is log expression, already normalized
-  if (normalize_counts==F) {
-    design <- X
-#    Y <- DGEList(Y)
-#    dge <- edgeR::calcNormFactors(Y)
-#    v <- voom(dge,design,plot=FALSE)
-    fit <- lmFit(Y,design, weights=NULL)
-    fit.ebayes <- eBayes(fit)
-  }
+  # default is TMM normalization
+   design <- X
+   v <- voom(Y,design,plot=FALSE)
+   fit <- lmFit(v,design)
+   fit.ebayes <- eBayes(fit)
 
   # given that the condition is a binary vector
   # extract the coefficient corresponds to the difference between the two conditions
